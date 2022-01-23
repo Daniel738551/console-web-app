3d684b000074912793a32888bcefd1b45452b181
Daniel738551 <88622092+Daniel738551@users.noreply.github.com>
2022-01-23 09:34:16 -0500

    Add files via upload

---
+++ c-77 console web app/main.js
@@ -0,0 +1,15 @@
+console.log("My first console");
+var first_name = "gregerny";
+var last_name = "johnson";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var x = 10;
+var y = 5;
+var add=x+y;
+console.log(add);
+var sub=x-y;
+console.log(sub);
+var mul=x*y;
+console.log(mul);
+var div=x/y;
+console.log(div);
